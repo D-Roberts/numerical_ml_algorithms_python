2cb7520dd656d0ef814ecd8013cc82a0352b2734
Denisa Roberts <d.roberts@vt.edu>
2022-07-30 10:07:08 -0400

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,69 +1,58 @@
-#!/usr/bin/env python3
-# -- * -- utf-8 -- * --
+from collections import Counter 
+from heapq import *
 
-"""3-nn
+import math
+import random
 
-Very simple 2 class implementation.
+random.seed(42)
 
-Majority vote by dist.
+def euclid(x1, x2):
+    return math.sqrt(sum(pow(a-b, 2) for a, b in zip(x1, x2)))
 
-function interpolation f(x) = y.
-"""
+def get_train_test(X, y, frac=0.8, seed=42):
+    inds = random.sample(range(len(X)), len(X))
+    train = int(frac * len(X)) 
+    return (X[:train], y[:train], X[train:], y[train:])
 
+def accuracy(y_true, preds):
+    return sum(a==b for a, b in zip(y_true, preds))/len(y_true)
 
-import numpy as np
+class KNN:
+    def __init__(self, k):
+        self.k = k 
 
-np.random.seed(1234)
+    def fit_predict(self, X_train, y_train, X_test):
+        preds = []
 
-def get_dist(x1, x2):
-	"""Euclid dist bet 2 vecs
-
-	"""
-	return np.sqrt(np.sum((x1 - x2) ** 2))
-
-
-class KNN(object):
-	def __init__(self, X, y, k=3):
-		self.X_train = X
-		self.y_train = y
-		self.k = k
-
-	def predict(self, x):
-		"""Make pred/interpolate at new point.
-
-		"""
-		M = self.X_train.shape[0]
-		x = np.asarray(x)
-
-		dist = []
-
-		for i in range(M):
-			d = get_dist(self.X_train[i, :], x)
-			dist.append(d)
-			# order is equal to indeces i
-
-		# sort and get lowest k indices
-		
-		topk_ind = np.argsort(dist)[:self.k]
-		vote = np.bincount(self.y_train[topk_ind]).argmax()
-		print(np.bincount(self.y_train[topk_ind]))
-
-		return vote
-
+        for sample in X_test:
+            pq = []
+            for i, row in enumerate(X_train):
+                dist = euclid(sample, row)
+                if len(pq) == self.k and -pq[0][0] > dist:
+                    heappushpop(pq, (-dist, i))
+                elif len(pq) < self.k:
+                    heappush(pq, (-dist, i))
+            c = Counter([y_train[i] for k, i in pq])
+            # print(c.most_common(1))
+            preds.append(c.most_common(1)[0][0])
+        return preds 
 
 def main():
+    from sklearn import datasets 
+    from sklearn.neighbors import KNeighborsClassifier
 
-	# Examples serving as unittest
-	
-	X = np.random.normal(0, 1, size = 40).reshape((20, 2))
-	y = np.random.randint(2, size = 20)
-	# print(X)
-	# print(y)
-	
-	test = KNN(X, y, 3)
-	y_hat = test.predict([1, 2])
-	print(y_hat)
+    data = datasets.load_iris()
+    X, y = data.data, data.target
 
+    trainx, trainy, testx, testy = get_train_test(X, y)
 
-if __name__ == '__main__':
-	main()
+    knn = KNN(5)
+    preds = knn.fit_predict(trainx, trainy, testx)
+    print(accuracy(testy, preds))
+
+    # baseline
+    skl = KNeighborsClassifier(n_neighbors=5, p=2, metric="minkowski")
+    preds1 = skl.fit(trainx, trainy).predict(testx)
+    print(accuracy(testy, preds1))
+
+main()
